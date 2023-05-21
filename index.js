66cbd2ba8468250125fcb41307e6801c3f3fc6ad
rafid149 <rafidtahmid75@gmail.com>
2023-05-21 20:56:39 +0600

    delete operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -64,6 +64,13 @@ async function run() {
         res.send(uniqueCategory)
       })
 
+      app.delete( '/toyDelete/:id', async (req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await toyCollection.deleteOne(query);
+        res.send(result);
+
+      })
  
 
     // Ensures that the client will close when you finish/error
